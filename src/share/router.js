005687def68c2facaf13ec6d22c07bc873a005f6
Leekee01 <zeje23@naver.com>
2023-01-20 20:34:52 +0900

    add router

--- src/share/router.js
+++ src/share/router.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Detailpage } from "../pages/DetailPage/DetailPage";
+import { MainPage } from "../pages/MainPage/MainPage";
+import { Mypage } from "../pages/MyPage/Mypage";
+import Layout from "./Layout/Layout";
+import Login from "./pages/Login";
 
-export default function router() {
+const Router = () => {
   return (
-    <div>router</div>
-  )
-}
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/:id" element={<Detailpage />} />
+          <Route path="/my" element={<Mypage />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
