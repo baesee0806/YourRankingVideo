3e085b96cc24fe618dfb7852edc28f7e04885cdb
Leekee01 <zeje23@naver.com>
2023-01-24 19:51:27 +0900

    add footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Footer() {
-  return <div>Footer</div>;
-}
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <FtrContainer>
+        <Logo src={require("../assets/Logo.png")} />
+        <Ftrtext>쉬게해조: 배성완,조성아,신정근,이희경,김승우</Ftrtext>
+        <Ftrtext>ⓒCopyright2023. YourRanking All rights reserved.</Ftrtext>
+      </FtrContainer>
+    </StyledFooter>
+  );
+};
+
+export const Logo = styled.img`
+  height: 50px;
+  cursor: pointer;
+`;
+
+export const StyledFooter = styled.div`
+  height: 110px;
+  width: 100%;
+  display: flex;
+  margin-bottom: 20px;
+  background-color: grey;
+`;
+
+export const FtrContainer = styled.div`
+  margin-top: 10px;
+  width: 2000px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Ftrtext = styled.div``;
+export default Footer;
