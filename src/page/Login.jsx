a634014e1538dd8e5959af9b36edcf065f9b97dc
Leekee01 <zeje23@naver.com>
2023-01-26 16:23:47 +0900

    add axios post, get
    todo:유효성 검사추가

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,5 +1,134 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
 
-export default function Login() {
-  return <div>Login</div>;
-}
+const Login = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.get("http://localhost:3001/user", { email, password }).then((response) => {
+      console.log(response);
+      alert("로그인성공");
+      navigate("/");
+    });
+  };
+  return (
+    <LoginContainer>
+      <Logo src={require("../assets/Logo.png")} />
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <Id>
+          <Input
+            placeholder={"Email"}
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </Id>
+        <PassWord>
+          <Input
+            placeholder={"Password"}
+            value={password}
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </PassWord>
+
+        <ToSignUp onClick={() => navigate("/signUp")}>new Member?</ToSignUp>
+
+        <RedButton onClick={handleSubmit}>로그인</RedButton>
+      </Form>
+    </LoginContainer>
+  );
+};
+
+export const LoginContainer = styled.div`
+  padding: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 50px;
+`;
+
+export const Logo = styled.img`
+  width: 90px;
+  height: 50px;
+`;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+`;
+
+export const Id = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 20px;
+  width: 300px;
+`;
+export const PassWord = styled.div`
+  display: flex;
+
+  width: 300px;
+`;
+
+export const Input = styled.input`
+  border: none;
+
+  color: #555;
+  box-sizing: border-box;
+  font-size: 18px;
+  :focus-visible {
+    outline: none;
+  }
+  padding-left: 10px;
+  background-color: #ededed;
+  border-radius: 10px;
+  width: 300px;
+  height: 40px;
+  box-shadow: 5px 5px 5px lightgrey;
+`;
+export const ToSignUp = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 20px;
+  cursor: pointer;
+  color: grey;
+`;
+
+export const RedButton = styled.button`
+  border: none;
+  background-color: #c4302b;
+  border-radius: 5px;
+  width: 300px;
+  height: 40px;
+  color: white;
+  font-size: 15px;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+export const Button = styled.button`
+  background-color: white;
+  width: 250px;
+  height: 40px;
+  cursor: pointer;
+  margin-bottom: 10px;
+  border-radius: 5px;
+`;
+export default Login;
--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -1,5 +1,142 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
-export default function SignUp() {
-  return <div>SignUp</div>;
-}
+const SignUp = () => {
+  const navigate = useNavigate();
+  //user정보
+  const [nickName, setNickName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const SignUpSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:3001/user", { email, password, nickName }).then((response) => {
+      console.log(response);
+      alert("회원가입 성공!");
+      navigate("/login");
+    });
+  };
+  return (
+    <LoginContainer>
+      <Logo src={require("../assets/Logo.png")} />
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <Id>
+          <Input
+            placeholder={"Email"}
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </Id>
+        <PassWord>
+          <Input
+            placeholder={"Password"}
+            value={password}
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </PassWord>
+        <NickName>
+          <Input
+            placeholder={"Nickname"}
+            value={nickName}
+            type="nickName"
+            onChange={(e) => {
+              setNickName(e.target.value);
+            }}
+          />
+        </NickName>
+
+        <RedButton onClick={SignUpSubmit}>회원가입</RedButton>
+      </Form>
+    </LoginContainer>
+  );
+};
+
+export const LoginContainer = styled.div`
+  padding: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 50px;
+`;
+
+export const Logo = styled.img`
+  width: 90px;
+  height: 50px;
+`;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+`;
+
+export const Id = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 20px;
+  width: 300px;
+`;
+export const PassWord = styled.div`
+  display: flex;
+  padding-bottom: 20px;
+  width: 300px;
+`;
+
+export const NickName = styled.div`
+  display: flex;
+
+  width: 300px;
+`;
+
+export const Input = styled.input`
+  border: none;
+
+  color: #555;
+  box-sizing: border-box;
+  font-size: 18px;
+  :focus-visible {
+    outline: none;
+  }
+  padding-left: 10px;
+  background-color: #ededed;
+  border-radius: 10px;
+  width: 300px;
+  height: 40px;
+  box-shadow: 5px 5px 5px lightgrey;
+`;
+
+export const RedButton = styled.button`
+  border: none;
+  background-color: #c4302b;
+  border-radius: 5px;
+  width: 300px;
+  height: 40px;
+  color: white;
+  font-size: 15px;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+export const Button = styled.button`
+  background-color: white;
+  width: 250px;
+  height: 40px;
+  cursor: pointer;
+  margin-bottom: 10px;
+  border-radius: 5px;
+`;
+export default SignUp;
