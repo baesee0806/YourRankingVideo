9800712177c8d29927f9e0d546c5e058913d06d1
baesee <baesee@kakao.com>
2023-01-26 10:02:33 +0900

    Add modal animation 화면 어두워짐

--- src/component/MenuModal.jsx
+++ src/component/MenuModal.jsx
@@ -22,37 +22,96 @@ export const MenuModal = () => {
   return (
     <>
       {modalHandler && (
-        <MenuModalLayoutDiv>
-          {/* 메뉴 창 닫기 */}
-          <div>
-            <button
-              onClick={() => {
-                setModalHandler(false);
-              }}
-            >
-              x
-            </button>
-          </div>
-          {/* 페이지 이동 */}
-          <div>
-            <div>로그아웃</div>
-            <div>인기영상</div>
-            <div>최신영상</div>
-            <div>유튜브 TOP 10</div>
-            <img src="" alt="" />
-          </div>
-        </MenuModalLayoutDiv>
+        <>
+          <ScreenBackgroundDiv
+            onClick={() => {
+              setModalHandler(false);
+            }}
+          ></ScreenBackgroundDiv>
+          <MenuLayoutDiv>
+            {/* 메뉴 창 닫기 */}
+            <MenuModalBtnAreaDiv>
+              <MenuModalOutBtn
+                onClick={() => {
+                  setModalHandler(false);
+                }}
+              >
+                x
+              </MenuModalOutBtn>
+            </MenuModalBtnAreaDiv>
+            {/* 페이지 이동 */}
+            <MenuModalMovePageAreaDiv>
+              <MenuModalMovePageDiv>로그아웃</MenuModalMovePageDiv>
+              <MenuModalMovePageDiv>인기영상</MenuModalMovePageDiv>
+              <MenuModalMovePageDiv>최신영상</MenuModalMovePageDiv>
+              <MenuModalMovePageDiv>유튜브 TOP 10</MenuModalMovePageDiv>
+              <LogoImg src={require("../assets/Logo.png")} />
+            </MenuModalMovePageAreaDiv>
+          </MenuLayoutDiv>
+        </>
       )}
     </>
   );
 };
 
-const MenuModalLayoutDiv = styled.div`
-  width: 200px;
+const ScreenBackgroundDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+const MenuLayoutDiv = styled.div`
+  width: 300px;
   height: 100%;
   position: absolute;
   top: 0;
   right: 0;
   background: #c4302b;
+  animation: fadeInLeft 1s;
+  @keyframes fadeInLeft {
+    0% {
+      opacity: 0;
+      transform: translate3d(50%, 0, 0);
+    }
+    to {
+      opacity: 1;
+      transform: translateZ(0);
+    }
+  }
 `;
-// 스크롤 막기
+
+// x Btn Area
+const MenuModalBtnAreaDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-bottom: 15px;
+`;
+
+const MenuModalOutBtn = styled.div`
+  border: none;
+  font-size: 30px;
+  color: white;
+  margin-right: 10px;
+`;
+
+// Router Btn area
+
+const MenuModalMovePageAreaDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const MenuModalMovePageDiv = styled.div`
+  font-size: 25px;
+  color: white;
+  margin: 0 0 15px 15px;
+`;
+
+const LogoImg = styled.img`
+  width: 50%;
+  margin-left: 25px;
+`;
