d1d06c92ec97239e3a3077622be96950b6cbdcda
Leekee01 <zeje23@naver.com>
2023-01-26 16:32:42 +0900

    add axios post, get
    todo: 유효성검사, 로그인 회원가입 실패

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -98,13 +98,45 @@ export const PassWord = styled.div`
 
 export const NickName = styled.div`
   display: flex;
-
+  padding-bottom: 20px;
   width: 300px;
 `;
 
 export const Input = styled.input`
   border: none;
 
-export default function SignUp() {
-  return <div>SignUp</div>;
-}
+  color: #555;
+  box-sizing: border-box;
+  font-size: 18px;
+  :focus-visible {
+    outline: none;
+  }
+  padding-left: 10px;
+  background-color: #ededed;
+  border-radius: 10px;
+  width: 300px;
+  height: 40px;
+  box-shadow: 5px 5px 5px lightgrey;
+`;
+
+export const RedButton = styled.button`
+  border: none;
+  background-color: #c4302b;
+  border-radius: 5px;
+  width: 300px;
+  height: 40px;
+  color: white;
+  font-size: 15px;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+export const Button = styled.button`
+  background-color: white;
+  width: 250px;
+  height: 40px;
+  cursor: pointer;
+  margin-bottom: 10px;
+  border-radius: 5px;
+`;
+export default SignUp;
