6e7211fe9b48202d74c3945508c9610955612680
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-01-29 22:50:44 +0900

    fix 배포 주소 변경

--- src/API/youtube.js
+++ src/API/youtube.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const MY_Key = "AIzaSyDC2j0XqxERFfjHRWO8YOVj368kFoZ10SY";
 
+const MY_ADRESS = "https://darkened-tasty-airship.glitch.me";
+
 // 대한민국 유튜브 인기동영상 10개 get
 // https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&regionCode=kr&key=AIzaSyDC2j0XqxERFfjHRWO8YOVj368kFoZ10SY
 
@@ -14,33 +16,20 @@ const fetchLists = async () => {
 
 const fetchVideo = async () => {
   const { data } = await axios.get(
-    `http://localhost:3001/videos?_sort=createAt&_order=desc`
+    `https://darkened-tasty-airship.glitch.me/videos?_sort=createAt&_order=desc`
   );
   return data;
 };
 
-const fetchLikes = async () => {
-  const { data } = await axios.get(`http://localhost:3001/likes`);
+const fetchLikes = async ({ queryKey }) => {
+  const { data } = await axios.get(`${MY_ADRESS}/${queryKey}`);
   return data;
 };
 
-const fetchPopVideo = async () => {
-  const { data } = await axios.get(`http://localhost:3001/videos`);
+const fetchPopVideo = async ({ queryKey }) => {
+  const { data } = await axios.get(`${MY_ADRESS}/${queryKey}`);
   return data;
 };
 //인기순으로 안갖고와지면 http://localhost:3001/videos?_sort=likesCount&_order=desc사용
 
 export { fetchLists, fetchVideo, fetchLikes, fetchPopVideo };
-
-// import { fetchLists } from "../API/youtube";
-// import { useQuery } from "react-query";
-// const { isLoading, isError, data } = useQuery("items", fetchLists);
-
-// if (isLoading) {
-//   return alert("로딩중");
-// }
-
-// if (isError) {
-//   return alert("에러");
-// }
-// console.log(data.items.map((el) => el.id));
